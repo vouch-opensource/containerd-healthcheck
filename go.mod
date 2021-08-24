824e5748c2a0c69e8c5c120b8f19da772828d48d
Raphael P. Ribeiro <raphael.ribeiro@vouch.io>
2021-08-24 08:48:34 -0300

    add containerd module

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/AppsFlyer/go-sundheit v0.5.0
+	github.com/containerd/containerd v1.5.5
 	github.com/ilyakaznacheev/cleanenv v1.2.5
 	github.com/prometheus/client_golang v1.11.0
 	github.com/sirupsen/logrus v1.8.1
---
+++ internal/containerd/containerd.go
@@ -0,0 +1,116 @@
+package containerd
+
+import (
+	"context"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/cio"
+	"github.com/containerd/containerd/namespaces"
+	"github.com/sirupsen/logrus"
+)
+
+type Containerd struct {
+	Logger  *logrus.Logger
+	Client  *containerd.Client
+	Context context.Context
+}
+
+func NewClient(logger *logrus.Logger, socketPath string, namespace string) (*Containerd, error) {
+	client, err := containerd.New(socketPath)
+	ctx := namespaces.WithNamespace(context.Background(), namespace)
+	if err != nil {
+		logger.Error("unable to connect to the containerd socket:", err)
+		return nil, err
+	}
+	return &Containerd{
+		Client:  client,
+		Context: ctx,
+		Logger:  logger,
+	}, nil
+}
+
+func (c *Containerd) RestartTask(containerName string) error {
+	ctx := c.Context
+	container, err := c.Client.LoadContainer(ctx, containerName)
+	if err != nil {
+		c.Logger.Error("unable to connect to the containerd socket")
+		return err
+	}
+
+	if err := c.stopTask(container); err != nil {
+		return err
+	}
+
+	if err := c.startTask(container); err != nil {
+		return err
+	}
+
+	time.Sleep(3 * time.Second)
+
+	return nil
+
+}
+
+func (c *Containerd) startTask(container containerd.Container) error {
+	ctx := c.Context
+	task, err := container.NewTask(ctx, cio.NullIO)
+	if err != nil {
+		return err
+	}
+	if err := task.Start(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Containerd) stopTask(container containerd.Container) error {
+	ctx := c.Context
+	task, err := container.Task(ctx, nil)
+	if err != nil {
+		if !strings.Contains(err.Error(), "no running task") {
+			return err
+		}
+		return err
+	}
+	status, err := task.Status(ctx)
+	switch status.Status {
+	case containerd.Stopped:
+		_, err := task.Delete(ctx)
+		if err != nil {
+			return err
+		}
+	case containerd.Running:
+		c.Logger.Debug(">>> 1")
+		statusC, err := task.Wait(ctx)
+		if err != nil {
+			c.Logger.Errorf("container %q: error during wait: %v", container.ID(), err)
+		}
+		c.Logger.Debug(">>> 2")
+		if err := task.Kill(ctx, syscall.SIGKILL); err != nil {
+			task.Delete(ctx)
+			return err
+		}
+		status := <-statusC
+		code, _, err := status.Result()
+		c.Logger.Debug(">>> 3")
+		if err != nil {
+			c.Logger.Errorf("container %q: error getting task result code: %v", container.ID(), err)
+			return err
+		}
+		if code != 0 {
+			c.Logger.Debugf("%s: exited container process: code: %d", container.ID(), status.ExitCode())
+		}
+		_, err = task.Delete(ctx)
+		c.Logger.Debug(">>> 4")
+		if err != nil {
+			return err
+		}
+	case containerd.Paused:
+		c.Logger.Errorf("Can't stop a paused container; unpause first")
+		return err
+	}
+	return nil
+}
